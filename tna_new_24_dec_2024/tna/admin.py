e2fcaea0dbfc8281c429e09d4e4ce405137d42a0
shivamdkc <shivamdkcwork@gmail.com>
2025-01-02 18:27:59 +0530

    daily last commit

--- tna_new_24_dec_2024/tna/admin.py
+++ tna_new_24_dec_2024/tna/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Task, Notification
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
--- tna_new_24_dec_2024/tna/models.py
+++ tna_new_24_dec_2024/tna/models.py
@@ -3,24 +3,24 @@ from django.db import models
 from django.core.validators import MinValueValidator
 
 class Role(models.Model):
-    role_name = models.CharField()
+    role_name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.role_name
 
 class User(models.Model):
-    username = models.CharField()
-    password = models.CharField()  # Ensure this field is used to store hashed passwords
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)  # Ensure this field is used to store hashed passwords
     email = models.EmailField()
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    status = models.CharField(max_length=50)  # e.g., Active, Inactive
+    status = models.CharField(max_length=200)  # e.g., Active, Inactive
 
     def __str__(self):
         return self.username
 
 class OrderDetail(models.Model):
     order_id = models.AutoField(primary_key=True) # Auto-incrementing primary key
-    style_name = models.CharField()
+    style_name = models.CharField(max_length=200)
     merchant = models.ForeignKey(User, related_name='merchant_orders', on_delete=models.CASCADE)
     qa = models.ForeignKey(User, related_name='qa_orders', on_delete=models.CASCADE)
     vendor = models.ForeignKey(User, related_name='vendor_orders', on_delete=models.CASCADE)
@@ -33,7 +33,7 @@ class OrderDetail(models.Model):
 class Task(models.Model):
     task_id = models.AutoField(primary_key=True) # Auto-incrementing primary key
     order = models.ForeignKey(OrderDetail, on_delete=models.CASCADE)
-    name = models.CharField()
+    name = models.CharField(max_length=200)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField() # This is the planned end date, so in begining it will take reference from just end date and then extension + end date. 
@@ -45,8 +45,8 @@ class Task(models.Model):
 class SubTask(models.Model):
     subtask_id = models.AutoField(primary_key=True) # Auto-incrementing primary key
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    sub_task_name = models.CharField()
-    type_of_work = models.CharField()
+    sub_task_name = models.CharField(max_length=200)
+    type_of_work = models.CharField(max_length=200)
     original_start_date = models.DateField()
     min_days_required = models.IntegerField()
     original_end_date = models.DateField()
@@ -55,51 +55,58 @@ class SubTask(models.Model):
         return self.sub_task_name
 
 class Extension(models.Model):
+
+    EXTENSION_STATUS_CHOICES = {
+        "P":"Pending",
+        "A":"Approved",
+        "R":"Rejected",
+    }
+
     extension_id = models.AutoField(primary_key=True) # Auto-incrementing primary key
     sub_task = models.ForeignKey(SubTask, on_delete=models.CASCADE)
     admin_approval = models.BooleanField( default=False)
     extension_days = models.PositiveIntegerField(validators=[MinValueValidator(1)])
     created_at = models.DateTimeField(auto_now_add=True)
     requested_by = models.ForeignKey(User, on_delete=models.CASCADE)
-    status = models.()  # e.g., Pending, Approved, Rejected
+    status = models.CharField(max_length=200,choices=EXTENSION_STATUS_CHOICES)  # e.g., Pending, Approved, Rejected
 
 class SubTaskCompleteDetail(models.Model):
-    id = models.CharField( primary_key=True)
-    sub_task = models.ForeignKey(SubTask, on_delete=models.CASCADE)
-    date_when_completed = models.DateField()
-    remarks = models.TextField()
+    id = models.CharField(max_length=200, primary_key=True)
+    sub_task_id = models.ForeignKey(SubTask, on_delete=models.CASCADE)
+    date_when_completed = models.DateField(null=False,blank=False)
+    remarks = models.TextField(null=True)
 
 class FabricDetail(models.Model):
-    id = models.CharField( primary_key=True)
+    fabric_id = models.CharField(max_length=200 ,primary_key=True)
     order = models.ForeignKey(OrderDetail, on_delete=models.CASCADE)
-    fabric_name = models.CharField()
+    fabric_name = models.CharField(max_length=200)
 
 class EmployeeDetail(models.Model):
-    id = models.CharField( primary_key=True)
-    name = models.CharField()
-    department = models.CharField()
-    designation = models.CharField()
-    role = models.CharField()
+    employee_id = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+    designation = models.CharField(max_length=200)
+    role = models.CharField(max_length=200)
 
 class PoDetail(models.Model):
-    id = models.CharField( primary_key=True)
+    id = models.CharField(max_length=200 ,primary_key=True)
     order = models.ForeignKey(OrderDetail, on_delete=models.CASCADE)
     po_number = models.IntegerField()
 
 class Notification(models.Model):
-    id = models.CharField( primary_key=True)
+    id = models.CharField(max_length=200, primary_key=True)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 class UserNotification(models.Model):
-    id = models.CharField( primary_key=True)
+    id = models.CharField(max_length=200, primary_key=True)
     user = models.ForeignKey(EmployeeDetail, on_delete=models.CASCADE)
     notification = models.ForeignKey(Notification, on_delete=models.CASCADE)
     is_read = models.BooleanField(default=False)
     notification_delivered_at = models.DateTimeField()
 
 class NotificationHistory(models.Model):
-    id = models.CharField( primary_key=True)
+    id = models.CharField(max_length=200, primary_key=True)
     user_notification = models.ForeignKey(UserNotification, on_delete=models.CASCADE)
     status = models.BooleanField()  # e.g., Delivered, Failed
     timestamp = models.DateTimeField(auto_now_add=True)
